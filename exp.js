2f000f6f66cd67c0d7f06a6ae0a1487862ebdcd4
al <a.k.sugiharto@gmail.com>
2020-03-25 12:45:25 +0100

    update questions

--- exp.js
+++ exp.js
@@ -26,79 +26,79 @@ var items = [ ["sep", "Separator", { }],
 ["intro", "Form", { html: { include: "example_intro.html" } }],
 
 ["startexp", "Message", {html: "This is the end of the practice session. The experiment begins now."}],
-["practice", q, {q: example_question_text + question_text("practice_1"),
+["practice", q, {q: example_question_text + question_text("example_1"),
                  as: answer, hasCorrect: false,
                  instructions: ""}],
-["practice", q, {q: example_question_text + question_text("practice_2"),
+["practice", q, {q: example_question_text + question_text("example_2"),
                  as: answer, hasCorrect: false,
                  instructions: ""}],
-["practice", q, {q: example_question_text + question_text("practice_3"),
+["practice", q, {q: example_question_text + question_text("example_3"),
                  as: answer, hasCorrect: false,
                  instructions: ""}],
 
-[["exp-emotion-withemotion-item1", 1], q, {q: question_text("practice_2"), as: answer}],
-[["exp-emoji-withemoji-item1", 1], q, {q: question_text("practice_2"), as: answer}],
-[["exp-emotion-withoutemotion-item1", 1], q, {q: question_text("practice_2"), as: answer}],
-[["exp-emoji-withoutemoji-item1", 1], q, {q: question_text("practice_2"), as: answer}],
+[["exp_1_downright_emoji_with", 1], q, {q: question_text("exp_1_downright_emoji_with"), as: answer}],
+[["exp_1_downright_emoji_without", 1], q, {q: question_text("exp_1_downright_emoji_without"), as: answer}],
+[["exp_1_downright_emotion_with", 1], q, {q: question_text("exp_1_downright_emotion_with"), as: answer}],
+[["exp_1_downright_emotion_without", 1], q, {q: question_text("exp_1_downright_emotion_without"), as: answer}],
 
-[["exp-emotion-withemotion-item2", 2], q, {q: question_text("practice_2"), as: answer}],
-[["exp-emoji-withemoji-item2", 2], q, {q: question_text("practice_2"), as: answer}],
-[["exp-emotion-withoutemotion-item2", 2], q, {q: question_text("practice_2"), as: answer}],
-[["exp-emoji-withoutemoji-item2", 2], q, {q: question_text("practice_2"), as: answer}],
+[["exp_2_downleft_emoji_with", 2], q, {q: question_text("exp_2_downleft_emoji_with"), as: answer}],
+[["exp_2_downleft_emoji_without", 2], q, {q: question_text("exp_2_downleft_emoji_without"), as: answer}],
+[["exp_2_downleft_emotion_with", 2], q, {q: question_text("exp_2_downleft_emotion_with"), as: answer}],
+[["exp_2_downleft_emotion_without", 2], q, {q: question_text("exp_2_downleft_emotion_without"), as: answer}],
 
-[["exp-emotion-withemotion-item3", 3], q, {q: question_text("practice_2"), as: answer}],
-[["exp-emoji-withemoji-item3", 3], q, {q: question_text("practice_2"), as: answer}],
-[["exp-emotion-withoutemotion-item3", 3], q, {q: question_text("practice_2"), as: answer}],
-[["exp-emoji-withoutemoji-item3", 3], q, {q: question_text("practice_2"), as: answer}],
+[["exp_3_upleft_emoji_with", 3], q, {q: question_text("exp_3_upleft_emoji_with"), as: answer}],
+[["exp_3_upleft_emoji_without", 3], q, {q: question_text("exp_3_upleft_emoji_without"), as: answer}],
+[["exp_3_upleft_emotion_with", 3], q, {q: question_text("exp_3_upleft_emotion_with"), as: answer}],
+[["exp_3_upleft_emotion_without", 3], q, {q: question_text("exp_3_upleft_emotion_without"), as: answer}],
 
-[["exp-emotion-withemotion-item4", 4], q, {q: question_text("practice_2"), as: answer}],
-[["exp-emoji-withemoji-item4", 4], q, {q: question_text("practice_2"), as: answer}],
-[["exp-emotion-withoutemotion-item4", 4], q, {q: question_text("practice_2"), as: answer}],
-[["exp-emoji-withoutemoji-item4", 4], q, {q: question_text("practice_2"), as: answer}],
+[["exp_4_upright_emoji_with", 4], q, {q: question_text("exp_4_upright_emoji_with"), as: answer}],
+[["exp_4_upright_emoji_without", 4], q, {q: question_text("exp_4_upright_emoji_without"), as: answer}],
+[["exp_4_upright_emotion_with", 4], q, {q: question_text("exp_4_upright_emotion_with"), as: answer}],
+[["exp_4_upright_emotion_without", 4], q, {q: question_text("exp_4_upright_emotion_without"), as: answer}],
 
-[["exp-emotion-withemotion-item5", 5], q, {q: question_text("practice_2"), as: answer}],
-[["exp-emoji-withemoji-item5", 5], q, {q: question_text("practice_2"), as: answer}],
-[["exp-emotion-withoutemotion-item5", 5], q, {q: question_text("practice_2"), as: answer}],
-[["exp-emoji-withoutemoji-item5", 5], q, {q: question_text("practice_2"), as: answer}],
+[["exp_5_downleft_emoji_with", 5], q, {q: question_text("exp_5_downleft_emoji_with"), as: answer}],
+[["exp_5_downleft_emoji_without", 5], q, {q: question_text("exp_5_downleft_emoji_without"), as: answer}],
+[["exp_5_downleft_emotion_with", 5], q, {q: question_text("exp_5_downleft_emotion_with"), as: answer}],
+[["exp_5_downleft_emotion_without", 5], q, {q: question_text("exp_5_downleft_emotion_without"), as: answer}],
 
-[["exp-emotion-withemotion-item6", 6], q, {q: question_text("practice_2"), as: answer}],
-[["exp-emoji-withemoji-item6", 6], q, {q: question_text("practice_2"), as: answer}],
-[["exp-emotion-withoutemotion-item6", 6], q, {q: question_text("practice_2"), as: answer}],
-[["exp-emoji-withoutemoji-item6", 6], q, {q: question_text("practice_2"), as: answer}],
+[["exp_6_downright_emoji_with", 6], q, {q: question_text("exp_6_downright_emoji_with"), as: answer}],
+[["exp_6_downright_emoji_without", 6], q, {q: question_text("exp_6_downright_emoji_without"), as: answer}],
+[["exp_6_downright_emotion_with", 6], q, {q: question_text("exp_6_downright_emotion_with"), as: answer}],
+[["exp_6_downright_emotion_without", 6], q, {q: question_text("exp_6_downright_emotion_without"), as: answer}],
 
-[["exp-emotion-withemotion-item7", 7], q, {q: question_text("practice_2"), as: answer}],
-[["exp-emoji-withemoji-item7", 7], q, {q: question_text("practice_2"), as: answer}],
-[["exp-emotion-withoutemotion-item7", 7], q, {q: question_text("practice_2"), as: answer}],
-[["exp-emoji-withoutemoji-item7", 7], q, {q: question_text("practice_2"), as: answer}],
+[["exp_7_topright_emoji_with", 7], q, {q: question_text("exp_7_topright_emoji_with"), as: answer}],
+[["exp_7_topright_emoji_without", 7], q, {q: question_text("exp_7_topright_emoji_without"), as: answer}],
+[["exp_7_topright_emotion_with", 7], q, {q: question_text("exp_7_topright_emotion_with"), as: answer}],
+[["exp_7_topright_emotion_without", 7], q, {q: question_text("exp_7_topright_emotion_without"), as: answer}],
 
-[["exp-emotion-withemotion-item8", 8], q, {q: question_text("practice_2"), as: answer}],
-[["exp-emoji-withemoji-item8", 8], q, {q: question_text("practice_2"), as: answer}],
-[["exp-emotion-withoutemotion-item8", 8], q, {q: question_text("practice_2"), as: answer}],
-[["exp-emoji-withoutemoji-item8", 8], q, {q: question_text("practice_2"), as: answer}],
+[["exp_8_topleft_emoji_with", 8], q, {q: question_text("exp_8_topleft_emoji_with"), as: answer}],
+[["exp_8_topleft_emoji_without", 8], q, {q: question_text("exp_8_topleft_emoji_without"), as: answer}],
+[["exp_8_topleft_emotion_with", 8], q, {q: question_text("exp_8_topleft_emotion_with"), as: answer}],
+[["exp_8_topleft_emotion_without", 8], q, {q: question_text("exp_8_topleft_emotion_without"), as: answer}],
 
 
-["control-item1", q, {q: question_text("practice_3"), as: answer}],
+["control-item1", q, {q: question_text("filler_1"), as: answer}],
 
-["control-item2", q, {q: question_text("practice_3"), as: answer}],
+["control-item2", q, {q: question_text("filler_2"), as: answer}],
 
-["control-item3", q, {q: question_text("practice_3"), as: answer}],
+["control-item3", q, {q: question_text("filler_3"), as: answer}],
 
-["control-item4", q, {q: question_text("practice_3"), as: answer}],
+["control-item4", q, {q: question_text("filler_4"), as: answer}],
 
-["control-item5", q, {q: question_text("practice_3"), as: answer}],
+["control-item5", q, {q: question_text("filler_5"), as: answer}],
 
-["control-item6", q, {q: question_text("practice_3"), as: answer}],
+["control-item6", q, {q: question_text("filler_6"), as: answer}],
 
-["control-item7", q, {q: question_text("practice_3"), as: answer}],
+["control-item7", q, {q: question_text("filler_7"), as: answer}],
 
-["control-item8", q, {q: question_text("practice_3"), as: answer}],
+["control-item8", q, {q: question_text("filler_8"), as: answer}],
 
-["control-item9", q, {q: question_text("practice_3"), as: answer}],
+["control-item9", q, {q: question_text("filler_9"), as: answer}],
 
-["control-item10", q, {q: question_text("practice_3"), as: answer}],
+["control-item10", q, {q: question_text("filler_10"), as: answer}],
 
-["control-item11", q, {q: question_text("practice_3"), as: answer}],
+["control-item11", q, {q: question_text("filler_11"), as: answer}],
 
-["control-item12", q, {q: question_text("practice_3"), as: answer}]
+["control-item12", q, {q: question_text("filler_12"), as: answer}]
 
 ];
